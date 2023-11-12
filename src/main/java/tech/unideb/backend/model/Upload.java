b17788bbd89f589d77a579592bba6bc2760d359a
Solymi <solymosiati001220@gmail.com>
2023-11-13 00:23:55 +0100

    IMG-12 feat: Create Upload

---
+++ src/main/java/tech/unideb/backend/model/Upload.java
@@ -0,0 +1,18 @@
+package tech.unideb.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "uploads")
+public class Upload {
+    @Id
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "uuid")
+    private User uploader;
+    private String fileName;
+    private ZonedDateTime uploadDate;
+    private long size;
+}
