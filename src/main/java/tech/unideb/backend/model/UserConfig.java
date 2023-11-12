86bc249e737e6fb3a39c0f7e0c748947aafafcc4
Solymi <solymosiati001220@gmail.com>
2023-11-13 00:51:07 +0100

    IMG-12 feat: Create UserConfig

---
+++ src/main/java/tech/unideb/backend/model/UserConfig.java
@@ -0,0 +1,15 @@
+package tech.unideb.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_configs")
+public class UserConfig {
+    @Id
+    private long id;
+    private String uploadSecret;
+    private boolean embedEnabled;
+    private String embedColor;
+    private String embedTitle;
+    private String embedDescription;
+}
