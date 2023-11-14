dac6b3c2fe69e30889feeae52143d988e312523c
MrExplode <me@sunstorm.rocks>
2023-11-14 23:46:38 +0100

    IMG-21 feat: File based storage

--- src/main/java/tech/unideb/backend/service/StorageService.java
+++ src/main/java/tech/unideb/backend/service/StorageService.java
@@ -3,13 +3,15 @@ package tech.unideb.backend.service;
 import org.springframework.core.io.Resource;
 import tech.unideb.backend.model.Upload;
 
+import java.io.IOException;
+
 public interface StorageService {
 
-    default void init() {}
+    default void init() throws IOException {}
 
-    void store(Upload upload, Resource data);
+    void store(Upload upload, Resource data) throws IOException;
 
-    Resource load(Upload upload);
+    Resource load(Upload upload) throws IOException;
 
-    void delete(Upload upload);
+    void delete(Upload upload) throws IOException;
 }
