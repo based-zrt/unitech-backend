b514bb96a7c09d721b131c5ff06d5d670d9a03a8
MrExplode <me@sunstorm.rocks>
2023-11-28 10:20:28 +0100

    feat: Bump version to release

--- src/main/java/tech/unideb/backend/dto/InviteDto.java
+++ src/main/java/tech/unideb/backend/dto/InviteDto.java
@@ -2,5 +2,11 @@ package tech.unideb.backend.dto;
 
 import java.time.ZonedDateTime;
 
-public record InviteDto(String key, ZonedDateTime createdAt, String creator, ZonedDateTime expiryDate, int uses, int maxUses) {
-}
+public record InviteDto(
+        String key,
+        ZonedDateTime createdAt,
+        String creator,
+        ZonedDateTime expiryDate,
+        int uses,
+        int maxUses
+) { }
